d956a1302296df662416f4918b4e8aeee0a64532
Ilona Poltavets <20ilona.poltavets02@gmail.com>
2026-02-20 17:14:09 +0200

    fix for ngrok

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,6 +12,16 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        // Respect forwarded proto/host when app runs behind ngrok or other reverse proxies.
+        $middleware->trustProxies(
+            at: '*',
+            headers: Request::HEADER_X_FORWARDED_FOR
+                | Request::HEADER_X_FORWARDED_HOST
+                | Request::HEADER_X_FORWARDED_PORT
+                | Request::HEADER_X_FORWARDED_PROTO
+                | Request::HEADER_X_FORWARDED_AWS_ELB
+        );
+
         $middleware->alias([
             'admin' => \App\Http\Middleware\AdminOnly::class,
             'admin_or_owner' => \App\Http\Middleware\AdminOrOwner::class,
